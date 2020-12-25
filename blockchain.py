a0e939f9690edcbc32cdf227c9f4b6ce89247061
Vivan <vivanvatsa@gmail.com>
2020-12-26 00:24:18 +0530

    init commit

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+from hashlib import sha256
+
+
+class Block:
+    data = None
+    hash = None
+    nonce = 0
+    previous_hash = "0" * 64
+
+
+class Blockchain:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
