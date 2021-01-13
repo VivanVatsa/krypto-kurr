34a8a0f531e0323e66634c6846f51b6aa6ae9e39
Vivan <vivanvatsa@gmail.com>
2021-01-13 09:33:49 +0530

    fixing CMAKE

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Vivan/Documents/GitHub/kal-el-krypto
-# Build directory: C:/Users/Vivan/Documents/GitHub/kal-el-krypto/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
